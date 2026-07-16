78c402598818bcaeed73152e01520897d006b0ca
nyx-Anna <jhaanju236@gmail.com>
2026-07-16 21:39:47 +0530

    Remove default Vite styles and setup global CSS

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,14 @@
 function Hero() {
   return (
-    <section className="min-h-screen bg-[#10002B] text-white pt-30 ">
+    <section className="min-h-screen bg-[#10002B] pt-30 ">
       <div className="max-w-7xl mx-auto w-full px-8 py-16">
         <div className="flex flex-col md:flex-row items-center justify-between">
           {/* Left Side */}
-          <div className="md:w-1/2 space-y-6">
-            <p>Hi, I'm</p>
-            <h1>Anjali Jha</h1>
-            <h2>Frontend Developer </h2>
-            <p>
+          <div className="md:w-1/2 space-y-8">
+            <p className="text-grey-400">Hi, I'm</p>
+            <h1 className="text-4xl">Anjali Jha</h1>
+            <h2 className="text-red-400">Frontend Developer</h2>
+            <p className="text-grey-400">
               Building clean, responsive interfaces where design meets
               functionality.
             </p>
